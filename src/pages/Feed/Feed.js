96fa4e571de5c80b1eb8a22e3a3f07cc7bcd07fb
Olateru Olu <oluolateru@gmail.com>
2024-01-14 01:03:46 +0100

    wrap up

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -22,8 +22,11 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch('URL')
-      .then(res => {
+    fetch('http://localhost:8080/auth/user/status', {
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
+    }).then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch user status.');
         }
@@ -50,7 +53,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts?page=' + page)
+    fetch('http://localhost:8080/feed/posts?page=' + page, {
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch posts.');
@@ -123,7 +130,10 @@ class Feed extends Component {
 
     fetch(url, {
       method: method,
-      body: formData
+      body: formData,
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -176,7 +186,10 @@ class Feed extends Component {
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
     fetch('http://localhost:8080/feed/post/' + postId, {
-      method: 'DELETE'
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
